07350cca16343718db8224f68dcb28d1a5423c7c
MinasMinasyan <minas9898@mail.ru>
2023-04-09 12:17:48 +0400

    Added new file:first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+let value = null;
+value = prompt("input a number");
+
+
+
+
+
+
+
+
+
+// tnayin ashxatanq 2
+
+
+// if(value >=18) {
+// alert(`duq ${value} tarekan eq ev karox eq qvyarkel`)
+// }else {
+//     alert('qvyarkelu tariq@ 18')
+// }
+
+
+// tnayin ashxatanq 3
+
+
+// if(value>=90 && value<=100) {
+// alert(`duq havaqel eq ${value} miavor dzer gnahatakann e A`)
+// }else if(value>=80 && value<=89){
+// alert(`duq havaqel eq ${value} miavor dzer gnahatakann e B`)
+// }else if(value>=70 && value<=79) {
+//     alert(`duq havaqel eq ${value} miavor dzer gnahatakann e Q`)
+// }else if(value>=60 && value<=69) {
+// alert(`duq havaqel eq ${value} miavor dzer gnahatakann e D`)
+// }else if(value<60){
+//     alert(`duq havaqel eq ${value} miavor dzer gnahatakann e F`)
+// }else {
+//     alert(`greq tiv`)
+// }
+
+// if(value/2!=+true){
+// alert('tiv@ kent e')
+// }else if(value/2==+true) {
+//     alert('tiv@ zuyg e')
+// }
+
+// tnayin ashxatanq 4
+// switch(+value){
+//     case 1:
+//     alert('erkushabti ashxatanqayin or');
+//     break;
+//     case 2:
+//         alert('ereqshabti ashxatanqayin or');
+//         break;
+//         case 3:
+//             alert('choreqshabti ashxatanqayin or');
+//             break;
+//             case 4:
+//                 alert('hingshabti ashxatanqayin or');
+//                 break;
+//                 case 5:
+//                     alert('urbat ashxatanqayin or');
+//                     break;
+//                     case 6:
+//                         alert('shabat voch ashxatanqayin or');
+//                         break;
+//                         case 7:
+//                             alert('kiraki voch ashxatanqayin or');
+//                             break;
+//                             default:
+//                                 alert('greq 1-7 tver@');
+//                                 break;
+
+// }
+
+
+//tnayin ashxatanq 6
+
+
+// if(value>=18 && value<=25){
+//     alert('nvazaguyn tariq')
+// }else{
+//     alert('duq cheq hamapatasxanum nvazaguyn tariqin')
+// }
